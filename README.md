59e4cef0cee4e7666b9a01985ca36d72fd12c223
ly931126 <1056548637@qq.com>
2017-07-25 09:16:58 +0800

    modify README.md

--- README.md
+++ README.md
@@ -35,23 +35,22 @@ newSingleThreadExecutor 创建一个单线程化的线程池，它只会用唯�
 ##### (1). newCachedThreadPool
 创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。示例代码如下：
 ```
-ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
-for (int i = 0; i < 10; i++) {
- final int index = i;
- try {
- Thread.sleep(index * 1000);
- } catch (InterruptedException e) {
- e.printStackTrace();
- }
+ExecutorService cachedThreadpool = Executors.newCachedThreadPool();
+		for (int i = 0; i < 10; i++) {
+			final int index = i;
+			cachedThreadpool.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Log.e("老鼠爱大米", String.valueOf(index));
+						Thread.sleep(index * 1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 
- cachedThreadPool.execute(new Runnable() {
-
- @Override
- public void run() {
- System.out.println(index);
- }
- });
-}
+				}
+			});
+		}
 ```
 线程池为无限大，当执行第二个任务时第一个任务已经完成，会复用执行第一个任务的线程，而不用每次新建线程。
 
--- app/src/main/java/com/example/test/testthread/MainActivity.java
+++ app/src/main/java/com/example/test/testthread/MainActivity.java
@@ -10,99 +10,100 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ExecutorService mSingleExecutor = null;
-    private static final long SLEEP_TIME = 2000;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//		new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				Toast.makeText(MainActivity.this, "早上好，么么哒，我爱你", Toast.LENGTH_LONG).show();
-//			}
-//		}).start();
-//        initSingleThreadExecutor();
-//        initCachedThreadPool();
-//        initFixedThreadPool();
-        initScheduledThreadPool();
-    }
-
-    private void initScheduledThreadPool() {
-        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
-//        for(int i=0;i<11;i++) {
-        scheduledThreadPool.schedule(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("y延迟3秒执行，我要变成一个大富婆，包养全天下的小鲜肉");
-            }
-        }, 3, TimeUnit.SECONDS);
-//        }
-
-//        scheduledThreadPool.scheduleAtFixedRate(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("延迟一秒后，每3秒执行一次");
-//            }
-//        }, 1, 3, TimeUnit.SECONDS);
-    }
-
-    private void initFixedThreadPool() {
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
-        for (int i = 0; i < 10; i++) {
-            final int index = i;
-            fixedThreadPool.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Log.e("你是那个对的人吗", String.valueOf(index));
-                        Thread.sleep(SLEEP_TIME);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-    }
-
-    private void initCachedThreadPool() {
-        ExecutorService cachedThreadpool = Executors.newCachedThreadPool();
-        for (int i = 0; i < 10; i++) {
-            final int index = i;
-
-            try {
-                Thread.sleep(index * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            cachedThreadpool.execute(new Runnable() {
-                @Override
-                public void run() {
-                    Log.e("老鼠爱大米", String.valueOf(index));
-                }
-            });
-        }
-    }
-
-    private void initSingleThreadExecutor() {
-        //实例化单一线程池
-        mSingleExecutor = Executors.newSingleThreadExecutor();
-        for (int i = 0; i < 10; i++) {
-            final int index = i;
-            mSingleExecutor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Log.e("你好，思密达", String.valueOf(index));
-                        Thread.sleep(SLEEP_TIME);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-
-    }
+	
+	private ExecutorService		mSingleExecutor	= null;
+	private static final long	SLEEP_TIME		= 2000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// new Thread(new Runnable() {
+		// @Override
+		// public void run() {
+		// Toast.makeText(MainActivity.this, "早上好，么么哒，我爱你",
+		// Toast.LENGTH_LONG).show();
+		// }
+		// }).start();
+		// initSingleThreadExecutor();
+		initCachedThreadPool();
+		// initFixedThreadPool();
+		// initScheduledThreadPool();
+	}
+	
+	private void initScheduledThreadPool() {
+		ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+		// for(int i=0;i<11;i++) {
+		scheduledThreadPool.schedule(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("y延迟3秒执行，我要变成一个大富婆，包养全天下的小鲜肉");
+			}
+		}, 3, TimeUnit.SECONDS);
+		// }
+		
+		// scheduledThreadPool.scheduleAtFixedRate(new Runnable() {
+		// @Override
+		// public void run() {
+		// System.out.println("延迟一秒后，每3秒执行一次");
+		// }
+		// }, 1, 3, TimeUnit.SECONDS);
+	}
+	
+	private void initFixedThreadPool() {
+		ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+		for (int i = 0; i < 10; i++) {
+			final int index = i;
+			fixedThreadPool.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Log.e("你是那个对的人吗", String.valueOf(index));
+						Thread.sleep(SLEEP_TIME);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+	}
+	
+	private void initCachedThreadPool() {
+		ExecutorService cachedThreadpool = Executors.newCachedThreadPool();
+		for (int i = 0; i < 10; i++) {
+			final int index = i;
+			cachedThreadpool.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Log.e("老鼠爱大米", String.valueOf(index));
+						Thread.sleep(index * 1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+				}
+			});
+		}
+	}
+	
+	private void initSingleThreadExecutor() {
+		// 实例化单一线程池
+		mSingleExecutor = Executors.newSingleThreadExecutor();
+		for (int i = 0; i < 10; i++) {
+			final int index = i;
+			mSingleExecutor.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Log.e("你好，思密达", String.valueOf(index));
+						Thread.sleep(SLEEP_TIME);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+		
+	}
 }
